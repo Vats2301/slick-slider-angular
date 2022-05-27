b77d8a7e34aedc5dac725e3aa7dd5477de91f43b
Vats2301 <vatsalpandya2301@gmail.com>
2022-05-27 14:22:06 +0530

    Updated responive settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,41 @@ import { Slick } from 'ngx-slickjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styles: [`.p-10 { padding: 10px }`]
+  styles: [`.p-10 { padding: 10px }`],
 })
 export class AppComponent {
-  arrayLength = 10;
+  // arrayLength = 10;
 
   config: Slick.Config = {
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      dots: true,
-      autoplay: true,
-      autoplaySpeed: 2000 
-    }
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    dots: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-  getArray(count: number) {
-    return new Array(count)
-  }
+  // getArray(count: number) {
+  //   return new Array(count);
+  // }
 }
